fa804ba496e8560bb2582bd11cd6cdb9565582e9
chabbasaad <dr.saadchabba@hotmail.fr>
2024-09-25 17:06:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.scss'
+import "@fontsource/poppins"; // Defaults to weight 400
+import "@fontsource/poppins/400.css"; // Specify weight
+import "@fontsource/poppins/500.css"; // Specify weight
+import "@fontsource/poppins/600.css"; // Specify weight
+import "@fontsource/poppins/700.css"; // Specify weight
+import "@fontsource/poppins/800.css"; // Specify weight
+import "@fontsource/poppins/900.css"; // Specify weight
